f15f4dd7c5fe66e47c625e26513628f061e07677
Tanishq Bakshi <tanishqbakshi118@gmail.com>
2023-11-03 08:05:59 +0530

    Add files via upload

---
+++ src/classes/Invoice.ts
@@ -0,0 +1,15 @@
+import { HasFormatter } from "../interfaces/HasFormatter.js";
+
+export class Invoice implements HasFormatter{
+   
+    constructor(
+        readonly client:string,
+        public details:string,
+        public amount:number)
+        {}
+
+    format() {
+        return `${this.client} owes ${this.amount} rupees for ${this.details}`;
+    }
+    
+}
---
+++ src/classes/Payment.ts
@@ -0,0 +1,15 @@
+import { HasFormatter } from "../interfaces/HasFormatter.js";
+
+export class Payment implements HasFormatter{
+   
+    constructor(
+        readonly recipient:string,
+        public details:string,
+        public amount:number)
+        {}
+
+    format() {
+        return `${this.recipient} is owed ${this.amount} rupees for ${this.details}`;
+    }
+    
+}
